e6edac3de5cb132a81952e248cf5bf283fb9e938
lokeshkumari-29-droid <lokeshkumar.i-29@scds.saiuniversity.edu.in>
2026-03-27 20:49:43 +0530

    Update reverse_string.c

--- reverse_string.c
+++ reverse_string.c
@@ -1,44 +1,33 @@
 #include <stdio.h>
 #include <string.h>
-
 #define MAX 100
-
 char stack[MAX];
-int top = -1;
 
+int top = -1;
 void push(char c) {
     if (top == MAX - 1)
         printf("Stack Overflow\n");
     else
         stack[++top] = c;
 }
-
 char pop() {
     if (top == -1)
         return '\0';
     else
         return stack[top--];
 }
-
 int main() {
     char str[100];
-
     printf("Enter string: ");
     scanf("%s", str);
-
     int len = strlen(str);
 	int i;
-    // Push all characters
     for (i = 0; i < len; i++) {
         push(str[i]);
     }
-
-    // Pop to reverse
-    for (wi = 0; i < len; i++) {
+    for (i = 0; i < len; i++) {
         str[i] = pop();
     }
-
     printf("Reversed string: %s\n", str);
-
     return 0;
 }
