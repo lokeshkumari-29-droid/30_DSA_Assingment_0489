9f390655a1ef7fb967cc1e9161b14da1bbebf975
lokeshkumari-29-droid <lokeshkumar.i-29@scds.saiuniversity.edu.in>
2026-03-27 20:46:50 +0530

    Update balanced_parantese.c

--- balanced_parantese.c
+++ balanced_parantese.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-
 #define MAX 100
-
 char stack[MAX];
-int top = -1;
 
+int top = -1;
 void push(char c) {
     stack[++top] = c;
 }
-
 void pop() {
     if (top != -1)
         top--;
 }
-
 int isEmpty() {
     return top == -1;
 }
-
 int main() {
     char exp[100];
-
     printf("Enter expression: ");
     scanf("%s", exp);
 	int i;
